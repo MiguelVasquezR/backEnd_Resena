cb528a430d4ab97aefd092a1fe2d425488fa0ff3
miguel <mvasquesrosas@gmail.com>
2023-12-21 20:07:32 -0600

    bd

---
+++ Autores.sql
@@ -0,0 +1,114 @@
+USE RESENAS;
+
+-- Gabriel García Márquez
+INSERT INTO InformacionPersonal VALUES ('18fdadbc-8b61-4d52-9b22-991a8f5e5c6d', 'Gabriel', 'García', 'Márquez', '1927-03-06', '2014-04-17', 'Escritor colombiano, premio Nobel de Literatura en 1982.');
+INSERT INTO Autor VALUES ('9a46b70e-cdd7-4b52-aa42-3a512c2f0f5d', 'Colombiano', '18fdadbc-8b61-4d52-9b22-991a8f5e5c6d');
+
+-- Julio Cortázar
+INSERT INTO InformacionPersonal VALUES ('7b79dd31-5cfc-4aef-9edc-6c7da5706e7f', 'Julio', 'Cortázar', 'Descotte', '1914-08-26', '1984-02-12', 'Escritor argentino, maestro del relato corto y la prosa poética.');
+INSERT INTO Autor VALUES ('fce5f648-ff97-44d9-aa5b-44d3e982f7ad', 'Argentino', '7b79dd31-5cfc-4aef-9edc-6c7da5706e7f');
+
+-- Octavio Paz
+INSERT INTO InformacionPersonal VALUES ('a8b77552-50e2-4a4d-86b3-1b062799e6b4', 'Octavio', 'Paz', 'Lozano', '1914-03-31', '1998-04-19', 'Poeta, ensayista y diplomático mexicano, premio Nobel de Literatura en 1990.');
+INSERT INTO Autor VALUES ('e057865e-6a64-4324-9da1-dc8b42e27a2d', 'Mexicano', 'a8b77552-50e2-4a4d-86b3-1b062799e6b4');
+
+-- Isabel Allende
+INSERT INTO InformacionPersonal VALUES ('3d180295-65c5-498e-bda9-4a2e1e2b39a7', 'Isabel', 'Allende', 'Llona', '1942-08-02', NULL, 'Escritora chilena, autora de "La casa de los espíritus".');
+INSERT INTO Autor VALUES ('c8f6b2ea-c5b3-428b-a5e9-03d65d3f6f19', 'Chilena', '3d180295-65c5-498e-bda9-4a2e1e2b39a7');
+
+-- Mario Vargas Llosa
+INSERT INTO InformacionPersonal VALUES ('cd1ea9g8-s9a7-4a6c-b7a5-ae01eda88', 'Mario', 'Vargas', 'Llosa', '1936-03-28', NULL, 'Escritor peruano, premio Nobel de Literatura en 2010.');
+INSERT INTO Autor VALUES ('cdfea0g1-s1a9-3a5c-b8a6-ae12eda90', 'Peruano', 'cd1ea9g8-s9a7-4a6c-b7a5-ae01eda88');
+
+-- Carlos Fuentes
+INSERT INTO InformacionPersonal VALUES ('cd1e1186-7a7e-4a6c-b7a5-ae01eaff437e', 'Carlos', 'Fuentes', 'Macías', '1928-11-11', '2012-05-15', 'Escritor mexicano, destacado representante de la literatura latinoamericana.');
+INSERT INTO Autor VALUES ('ffddc101-4f0e-43f8-aa5b-8c68390a6b3a', 'Mexicano', 'cd1e1186-7a7e-4a6c-b7a5-ae01eaff437e');
+
+-- Laura Esquivel
+INSERT INTO InformacionPersonal VALUES ('ee07b079-8c69-417a-b692-1b8bb601bc45', 'Laura', 'Esquivel', 'Valdés', '1950-09-30', NULL, 'Escritora y novelista mexicana, conocida por "Como agua para chocolate".');
+INSERT INTO Autor VALUES ('a0cb4a55-5b24-4e65-b485-0c0418e0f781', 'Mexicana', 'ee07b079-8c69-417a-b692-1b8bb601bc45');
+
+-- Juan Rulfo
+INSERT INTO InformacionPersonal VALUES ('9d202fc3-b9fc-4d23-b0ce-442f59840d62', 'Juan', 'Rulfo', 'Vizcaíno', '1917-05-16', '1986-01-07', 'Escritor mexicano, conocido por "Pedro Páramo" y "El llano en llamas".');
+INSERT INTO Autor VALUES ('7a9c8b14-682b-4ee1-8e90-50c554ce04c8', 'Mexicano', '9d202fc3-b9fc-4d23-b0ce-442f59840d62');
+
+-- Carlos Drummond de Andrade
+INSERT INTO InformacionPersonal VALUES ('3b84b6e9-dc99-4a06-b9ea-5a692a416ec0', 'Carlos', 'Drummond', 'de Andrade', '1902-10-31', '1987-08-17', 'Poeta brasileño, importante en la literatura del siglo XX.');
+INSERT INTO Autor VALUES ('fd52a226-88a8-44f2-8f5c-5316bf90e7f4', 'Brasileño', '3b84b6e9-dc99-4a06-b9ea-5a692a416ec0');
+
+-- Rosario Castellanos
+INSERT INTO InformacionPersonal VALUES ('eb8df5aa-1ad8-42b9-9da0-1271dbd8c687', 'Rosario', 'Castellanos', 'Figueroa', '1925-05-25', '1974-08-07', 'Escritora y poetisa mexicana, una de las voces más importantes de la literatura femenina.');
+INSERT INTO Autor VALUES ('5c114f5d-b11f-4b8c-89e4-2ad3e3f8a0f4', 'Mexicana', 'eb8df5aa-1ad8-42b9-9da0-1271dbd8c687');
+
+-- Jorge Luis Borges
+INSERT INTO InformacionPersonal VALUES ('4e1ad4ed-5f80-4c3f-9c68-47c7c7330a50', 'Jorge', 'Borges', 'Acevedo', '1899-08-24', '1986-06-14', 'Escritor argentino, autor de obras como "Ficciones" y "El Aleph".');
+INSERT INTO Autor VALUES ('f7dd37f4-18b1-4c85-b3ce-d9e7b117cb0d', 'Argentino', '4e1ad4ed-5f80-4c3f-9c68-47c7c7330a50');
+
+-- Pablo Neruda
+INSERT INTO InformacionPersonal VALUES ('e4da87b2-890d-4b0a-979f-b679a35dd60f', 'Pablo', 'Neruda', 'Ricardo Reyes', '1904-07-12', '1973-09-23', 'Poeta chileno, premio Nobel de Literatura en 1971.');
+INSERT INTO Autor VALUES ('078070ae-ba21-4c52-91f3-b4e3a071cd42', 'Chileno', 'e4da87b2-890d-4b0a-979f-b679a35dd60f');
+
+-- Alfonsina Storni
+INSERT INTO InformacionPersonal VALUES ('87193eb0-2ba0-4590-b37b-64db8e1c1266', 'Alfonsina', 'Storni', 'Quesad', '1892-05-29', '1938-10-25', 'Poeta argentina, destacada figura del modernismo.');
+INSERT INTO Autor VALUES ('af38439b-992f-40c1-b471-96f8d9d36c1a', 'Argentina', '87193eb0-2ba0-4590-b37b-64db8e1c1266');
+
+-- Julio Ramón Ribeyro
+INSERT INTO InformacionPersonal VALUES ('bfb3fe7b-fa6f-44f8-b349-21d781d3a00a', 'Julio Ramón', 'Ribeyro', 'Zúñiga', '1929-08-31', '1994-12-04', 'Escritor peruano, conocido por sus cuentos y crónicas.');
+INSERT INTO Autor VALUES ('55d4d6a5-6325-4e0f-b241-c7f8997ce14e', 'Peruano', 'bfb3fe7b-fa6f-44f8-b349-21d781d3a00a');
+
+-- Miguel de Unamuno
+INSERT INTO InformacionPersonal VALUES ('9f4bf65e-e10d-460c-bbc5-903d1ec660ed', 'Miguel', 'de Unamuno', 'y Jugo', '1864-09-29', '1936-12-31', 'Escritor y filósofo español, figura destacada de la Generación del 98.');
+INSERT INTO Autor VALUES ('f4dd2d87-6121-46e7-8da2-6f8716e0b72b', 'Español', '9f4bf65e-e10d-460c-bbc5-903d1ec660ed');
+
+-- Alejandra Pizarnik
+INSERT INTO InformacionPersonal VALUES ('c19a3db4-e794-4ef1-b6ab-fd0d881dbf27', 'Alejandra', 'Pizarnik', '', '1936-04-29', '1972-09-25', 'Poetisa y escritora argentina, conocida por su obra lírica y su poesía introspectiva.');
+INSERT INTO Autor VALUES ('8b1b51d1-7d1c-4c78-909d-1c6e1ee8c222', 'Argentina', 'c19a3db4-e794-4ef1-b6ab-fd0d881dbf27');
+
+-- Miguel de Cervantes
+INSERT INTO InformacionPersonal VALUES ('a5df92dd-2a3a-46b5-8329-ef189f75a00f', 'Miguel', 'de Cervantes', 'Saavedra', '1547-09-29', '1616-04-22', 'Escritor español, autor de "Don Quijote de la Mancha", considerada la primera novela moderna.');
+INSERT INTO Autor VALUES ('81a4c7a7-58d9-4ff7-986f-725a9e95020d', 'Español', 'a5df92dd-2a3a-46b5-8329-ef189f75a00f');
+
+-- Virginia Woolf
+INSERT INTO InformacionPersonal VALUES ('621e9ac6-b9db-41b4-a400-bf4885d5a597', 'Virginia', 'Woolf', 'Stephen', '1882-01-25', '1941-03-28', 'Escritora británica, figura importante en la literatura modernista, autora de "La señora Dalloway".');
+INSERT INTO Autor VALUES ('9d2140b1-51e0-4c62-87e5-cc2079a2802e', 'Británica', '621e9ac6-b9db-41b4-a400-bf4885d5a597');
+
+-- Franz Kafka
+INSERT INTO InformacionPersonal VALUES ('ab20155b-27a3-4e42-a0c3-8f80b5b3f9e1', 'Franz', 'Kafka', '', '1883-07-03', '1924-06-03', 'Escritor checo-alemán, conocido por sus obras como "La metamorfosis" y "El proceso".');
+INSERT INTO Autor VALUES ('8d88f9d5-2e87-4c4e-9fe6-cb5bdfb5f899', 'Checo-Alemán', 'ab20155b-27a3-4e42-a0c3-8f80b5b3f9e1');
+
+-- Agatha Christie
+INSERT INTO InformacionPersonal VALUES ('e1cda7a5-7ea5-460a-845e-e968b47f50e0', 'Agatha', 'Christie', 'Miller', '1890-09-15', '1976-01-12', 'Escritora británica de novelas policíacas, creadora del famoso detective Hercule Poirot.');
+INSERT INTO Autor VALUES ('0cb152c2-c8b7-4c80-99b5-2821ef5f77c8', 'Británica', 'e1cda7a5-7ea5-460a-845e-e968b47f50e0');
+
+-- Langston Hughes
+INSERT INTO InformacionPersonal VALUES ('944be3e7-b26b-4c01-a689-e3b21f40ef67', 'Langston', 'Hughes', '', '1902-02-01', '1967-05-22', 'Poeta, novelista y activista estadounidense, figura destacada del Renacimiento de Harlem.');
+INSERT INTO Autor VALUES ('6b87be42-4988-4c5d-82cf-2b9e9f1200fb', 'Estadounidense', '944be3e7-b26b-4c01-a689-e3b21f40ef67');
+
+-- Italo Calvino
+INSERT INTO InformacionPersonal VALUES ('23ef7676-1b1e-4a02-8c67-9f82c5d7e87c', 'Italo', 'Calvino', '', '1923-10-15', '1985-09-19', 'Escritor italiano, autor de "Las ciudades invisibles" y "El barón rampante".');
+INSERT INTO Autor VALUES ('a1d575ed-67ea-40c7-b579-005d3d4f9a1c', 'Italiano', '23ef7676-1b1e-4a02-8c67-9f82c5d7e87c');
+
+-- Gabriel Mistral
+INSERT INTO InformacionPersonal VALUES ('f50f1e5b-031c-4b77-bb91-3703fe6ac4b0', 'Gabriela', 'Mistral', 'Vicuña', '1889-04-07', '1957-01-10', 'Poetisa chilena, primera latinoamericana en recibir el Premio Nobel de Literatura en 1945.');
+INSERT INTO Autor VALUES ('0370fb8e-b8a4-4e76-9b1d-ba4c3bbf3b38', 'Chilena', 'f50f1e5b-031c-4b77-bb91-3703fe6ac4b0');
+
+-- John Steinbeck
+INSERT INTO InformacionPersonal VALUES ('32f3f61a-0e1a-4f8f-aa3f-4704fe93f4fe', 'John', 'Steinbeck', '', '1902-02-27', '1968-12-20', 'Escritor estadounidense, autor de "Las uvas de la ira" y "Al Este del Edén".');
+INSERT INTO Autor VALUES ('b85e4cd7-f603-4b6f-a008-b0806a6d5f1c', 'Estadounidense', '32f3f61a-0e1a-4f8f-aa3f-4704fe93f4fe');
+
+-- Anne Frank
+INSERT INTO InformacionPersonal VALUES ('c80c4ea6-c20a-4b18-9f90-d3035d1a154c', 'Anne', 'Frank', '', '1929-06-12', '1945-03-12', 'Diarista judía alemana, autora del famoso "Diario de Ana Frank" durante la Segunda Guerra Mundial.');
+INSERT INTO Autor VALUES ('f8485361-4d88-4935-ba76-55f3da2a7db4', 'Alemana', 'c80c4ea6-c20a-4b18-9f90-d3035d1a154c');
+
+-- Wole Soyinka
+INSERT INTO InformacionPersonal VALUES ('8e5bf10b-b540-47c1-974c-59b10190d06e', 'Wole', 'Soyinka', '', '1934-07-13', NULL, 'Dramaturgo y escritor nigeriano, primer africano en recibir el Premio Nobel de Literatura en 1986.');
+INSERT INTO Autor VALUES ('3c7a888d-e199-4012-aac6-bf33a13e5e9b', 'Nigeriano', '8e5bf10b-b540-47c1-974c-59b10190d06e');
+
+-- Amy Tan
+INSERT INTO InformacionPersonal VALUES ('bebc3b67-60a3-47d8-9db6-14e1ec08ed6d', 'Amy', 'Tan', '', '1952-02-19', NULL, 'Escritora estadounidense-china, autora de "El club de la buena estrella" y "El valle del asombro".');
+INSERT INTO Autor VALUES ('8a20f7d8-2f47-44aa-9e91-f5ac54f54594', 'Estadounidense-China', 'bebc3b67-60a3-47d8-9db6-14e1ec08ed6d');
+
+
+
+
+
